8e5afc46508031fef5c69bb445f972fa974fe38c
yevagav <yevagavrilov@gmail.com>
2023-01-18 19:00:23 -0800

    updated nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <nav>
-      <Link to="/orders">Order History</Link>
+       <Link to="">Fauxbook logo</Link>
+       &nbsp; | &nbsp;
+      <Link to="">Home</Link>
       &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
+      <Link to="">Profile Pic</Link>
     </nav>
   );
 }
