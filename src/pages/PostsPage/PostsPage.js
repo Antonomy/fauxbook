c03b2c0d4361955c71addbdf9a2024a547cbb87b
yevagav <yevagavrilov@gmail.com>
2023-01-18 19:52:13 -0800

    created photos preview and add it to the postspage: q

--- src/pages/PostsPage/PostsPage.js
+++ src/pages/PostsPage/PostsPage.js
@@ -1,8 +1,12 @@
 import styles from './PostsPage.module.scss'
 import PostForm from '../../components/PostForm'
+import PhotosPreview from '../../components/PhotosPreview/PhotosPreview'
 
 export default function PostsPage () {
     return(
+        <div>
         <PostForm />
+        <PhotosPreview />
+        </div>
     )
 }
