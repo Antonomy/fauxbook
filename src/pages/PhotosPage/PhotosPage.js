7148a29a8460bbe3d575e8ce2e5cb5bcd7b578b3
yevagav <yevagavrilov@gmail.com>
2023-01-23 15:26:02 -0800

    added components for profilenavbar

--- src/pages/PhotosPage/PhotosPage.js
+++ src/pages/PhotosPage/PhotosPage.js
@@ -1,9 +1,10 @@
 import styles from './PhotosPage.module.scss'
+import ProfileNavBar from '../../components/ProfileNavBar/ProfileNavBar'
 
 export default function PhotosPage () {
     return (
         <div>
-            
+            <ProfileNavBar />
         </div>
     )
 }
