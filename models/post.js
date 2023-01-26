7d02c42bdee316c2837aa4f938dfb047d3c4f215
Kenneth Presedo <kennethpresedo@live.com>
2023-01-25 22:45:46 -0500

    full crud on posts

--- models/post.js
+++ models/post.js
@@ -1,43 +1,44 @@
 const { Schema, model } = require('mongoose')
 
 const postSchema = new Schema({
-    post: { 
-        type: String,
-        required: true 
+    post: String
   },
+  //,
 
-    comments: { 
-        type: String,
-        required: false 
-  },
-    replies: {
-        type: String,
-        required: false
-  },
-    createdDate: {
-        type: Date,
-        default: Date.now
-    },
-    photo: {
-        type: String,
-        required: false 
-    },
-    video: {
-        type: String,
-        required: false
-    },
-    reaction: {
-     type: String,
-     required: false
-}
+//     comments: { 
+//         type: String,
+//         required: false 
+//   },
+//     replies: {
+//         type: String,
+//         required: false
+//   },
+//     createdDate: {
+//         type: Date,
+//         default: Date.now
+//     },
+//     photo: {
+//         type: String,
+//         required: false 
+//     },
+//     video: {
+//         type: String,
+//         required: false
+//     },
+//     reaction: {
+//      type: String,
+//      required: false
+// }
 
-}, {
+{
     timestamps: true
   })
 
-module.exports = model('Post', postSchema)
 
 
+const Post = model('Post', postSchema)
+
+module.exports = Post
 
 
 
