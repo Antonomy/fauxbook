4ba7a4e769402affba7b23ce8d21d778d99789f2
Antonomy <antonyyu@gmail.com>
2023-01-28 08:01:07 -0800

    updated About and Request

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,23 +1,27 @@
 import styles from './AboutPage.module.scss'
 import User from '../../components/User/User'
 import ProfileNavBar from '../../components/ProfileNavBar/ProfileNavBar'
+import editIcon from './images/editpencilicon.png'
 
-export default function AboutPage () {
+export default function AboutPage() {
     return (
         <div>
             <ProfileNavBar />
             <div>
-            <User />
-        </div>
-            <div>Bio - this is like a brand statement</div>
-            <div>Birthday</div>
-            <div>Workplace</div>
-            <div>High School</div>
-            <div>College</div>
-            <div>Current City</div>
-            <div>Hometown</div>
-            <div>Relationship Status</div>
-            <div>Workplace</div>
+                <User />
+            </div>
+            Name: 
+            <br />
+            Email: <img height='10' src={editIcon} />
+            <div>Bio - this is like a brand statement<img height='10' src={editIcon} /></div>
+            <div>Birthday<img height='10' src={editIcon} /></div>
+            <div>Workplace<img height='10' src={editIcon} /></div>
+            <div>High School<img height='10' src={editIcon} /></div>
+            <div>College<img height='10' src={editIcon} /></div>
+            <div>Current City<img height='10' src={editIcon} /></div>
+            <div>Hometown<img height='10' src={editIcon} /></div>
+            <div>Relationship Status<img height='10' src={editIcon} /></div>
+            <div>Workplace<img height='10' src={editIcon} /></div>
 
         </div>
     )
