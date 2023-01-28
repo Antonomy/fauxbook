f2a30fd76cdba1dab26d1a51b6989c62c9357888
Antonomy <antonyyu@gmail.com>
2023-01-28 10:41:35 -0800

    removed comments per Chris

--- models/post.js
+++ models/post.js
@@ -7,33 +7,6 @@ const postSchema = new Schema({
   },
 
 
-//     comments: { 
-//         type: String,
-//         required: false 
-//   },
-//     replies: {
-//         type: String,
-//         required: false
-//   },
-//     createdDate: {
-//         type: Date,
-//         default: Date.now
-//     },
-//     photo: {
-//         type: String,
-//         required: false 
-//     },
-//     video: {
-//         type: String,
-//         required: false
-//     },
-//     reaction: {
-//      type: String,
-//      required: false
-// }
-
-
-{
   user: [{ 
 
     type: Schema.Types.ObjectId, ref: 'User'
