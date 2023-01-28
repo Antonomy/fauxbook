88239d844dc31f3b6215e35f126c86b97b025e77
chrisIyer <hellohotbox420@gmail.com>
2023-01-28 12:39:22 -0500

    added routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,9 +2,8 @@
 // const router = express.Router()
 // const { dataController, apiController } = require('../../controllers/api/posts')
 
-// // add routes
-// // Index /api/posts
-// router.get('/', dataController.index, apiController.index)
+
+
 // // Index /api/posts/post/:id
 // router.get('/post/:id', dataController.postIndex, apiController.index)
 
@@ -34,6 +33,20 @@ router.put('/:id', dataController.update, apiController.show)
 router.post('/', dataController.create, apiController.show)
 // Show /api/posts/:id
 router.get('/:id', dataController.show, apiController.show)
+
+// // Index /api/posts/post/:id
+router.get('/post/:id', dataController.postIndex, apiController.index)
+
+// // Index /api/posts/user/:username
+router.get('/user/:username', dataController.userPostIndex, apiController.index)
+
+// // Index /api/posts/post/${id}/excludeUser/${username}
+router.get('/post/:id/excludeUser/:username', dataController.postExcludeUserIndex, apiController.index)
+
+// // Index /api/posts/post/:id/user/:username
+router.get('/post/:id/user/:username', dataController.userPostShow, apiController.index)
+
+
 
 
 module.exports = router
