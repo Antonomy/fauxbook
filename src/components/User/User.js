538592a9b937c194a981d7707884562a13c4bb6a
Kenneth Presedo <kennethpresedo@live.com>
2023-01-31 22:33:35 -0500

    About page and User component edits

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,6 +1,7 @@
 import styles from './User.module.scss'
 import { Link } from 'react-router-dom'
 import { useState } from "react";
+import AboutPage from '../../pages/AboutPage/AboutPage'
 
 export default function User(props) {
     return (
@@ -9,8 +10,15 @@ export default function User(props) {
                 <div>
                     <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" height="100" alt="photo" />
                 </div>
-                <li>NAME user.name</li>
-                <li>Current City user.currentCity</li>
+                <li>{User.firstName}</li>
+                <li>{User.lastName}</li>
+                <li>{User.email}</li>
+                <li>{User.birthday}</li>
+                <li>{User.highSchool}</li>
+                <li>{User.college}</li>
+                <li>{User.currentCity}</li>
+                <li>{User.hometown}</li>
+                <li>{User.relationshipStatus}</li>
             </div>
         </div>
     )
