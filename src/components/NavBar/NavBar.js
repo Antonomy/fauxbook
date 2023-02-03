d1dca808052aac17ae75d69ed6ee992d1c31afbe
Antonomy <antonyyu@gmail.com>
2023-02-03 14:27:00 -0800

    styling for navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,52 +11,50 @@ export default function NavBar({
   function handleLogOut() {
     logOut();
     setUser(null);
-}
+  }
   return (
     <nav className="nav">
       <nav className='body'>
-      <ul>
-        <li>
-       <Link to="/newsfeed" className='logo' class="tooltip" data-tooltip="Logo" id="btn_logo"><img height='40rem' src={logo} className='logo' /></Link>
-       &nbsp; | &nbsp;
-       </li>
-      <li>
-      <Link to="/newsfeed" class="tooltip" data-tooltip="Home" id="btn_home"><img height='25rem' src={homeIcon} /></Link>
-      &nbsp; | &nbsp;
-      </li>
-      <li>
-      <Link to={`/${user._id}/about`} class="tooltip" data-tooltip="About" id="btn_About" className='navbarlink'>About</Link>
-      &nbsp; | &nbsp;
-      </li>
-      <li>
-      <Link to="/friends" class="tooltip" data-tooltip="Friends" id="btn_friends" className='navbarlink'>All Users</Link>
-      &nbsp; | &nbsp;
-      </li>
-      <li>
-      <Link to="/requests" class="tooltip" data-tooltip="Requests" id="btn_requests" className='navbarlink'>Requests</Link>
-      &nbsp; | &nbsp;
-      </li>
-      <li>
-      <Link to="/photos" class="tooltip" data-tooltip="Photos" id="btn_photos" className='navbarlink'>Photos</Link>
-      &nbsp; | &nbsp;
-      </li>
-      <li>
-      <Link to={`/${user._id}/posts`} class="tooltip" data-tooltip="Profile" id="btn_profile" className='navbarlink'>{user.firstName} {user.lastName}'s Profile </Link>
-      &nbsp; | &nbsp;
-      </li>
-      <br />
-      {/* &nbsp;TO BE REMOVED - only here for convenience: &nbsp; */}
-      <br/>
-      
-      <li>
-      <Link to="/policy" class="tooltip" data-tooltip="Policy" id="btn_policy">Policy</Link>
-      &nbsp; | &nbsp;
-      </li>
-      <li>
-      <Link onClick={handleLogOut}>Log Out</Link>
-    </li>
-    </ul>
+        <ul>
+          <li>
+            <Link to="/newsfeed" className='logo' id="btn_logo"><img height='40rem' src={logo} className='logo' /></Link>
+          </li>
+          <li>
+            <Link to="/newsfeed" class="tooltip" data-tooltip="Home" id="btn_home"><img height='25rem' src={homeIcon} /></Link>
+
+          </li>
+          <li>
+            <Link to={`/${user._id}/about`} class="tooltip" data-tooltip="About" id="btn_About" className='navbarlink'>About</Link>
+
+          </li>
+          <li>
+            <Link to="/friends" class="tooltip" data-tooltip="Friends" id="btn_friends" className='navbarlink'>All Users</Link>
+
+          </li>
+          <li>
+            <Link to="/requests" class="tooltip" data-tooltip="Requests" id="btn_requests" className='navbarlink'>Requests</Link>
+
+          </li>
+          <li>
+            <Link to="/photos" class="tooltip" data-tooltip="Photos" id="btn_photos" className='navbarlink'>Photos</Link>
+
+          </li>
+          <li>
+            <Link to={`/${user._id}/posts`} class="tooltip" data-tooltip="Profile" id="btn_profile" className='navbarlink'>{user.firstName} {user.lastName}'s Profile </Link>
+
+          </li>
+          <br />
+          {/* &nbsp;TO BE REMOVED - only here for convenience: &nbsp; */}
+          <br />
+
+          <li>
+            <Link to="/policy" class="tooltip" data-tooltip="Policy" id="btn_policy">Policy</Link>
+          </li>
+          <li>
+            <Link onClick={handleLogOut} className='navbarlink' >Log Out</Link>
+          </li>
+        </ul>
+      </nav>
     </nav>
-        </nav>
   );
 }
