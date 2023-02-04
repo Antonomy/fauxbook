f0fb3330588822c53b66b3e06359ce83b7f3cc04
Antonomy <antonyyu@gmail.com>
2023-02-03 17:44:16 -0800

    updated user component styling

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -5,22 +5,19 @@ import AboutPage from '../../pages/AboutPage/AboutPage'
 
 export default function User ({ user }) {
   return (
-    <div>
-
       <div className={styles.friend}>
         <div>
           <img src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' height='100' alt='photo' />
         </div>
-        <li> <Link to={`/${user._id}/posts`}>{user.firstName} {user.lastName}</Link></li>
-        <li>{user.email}</li>
-        <li>{user.birthday}</li>
-        <li>{user.highSchool}</li>
-        <li>{user.college}</li>
-        <li>{user.currentCity}</li>
-        <li>{user.hometown}</li>
-        <li>{user.relationshipStatus}</li>
+        <li className={styles.userdetails}> <Link to={`/${user._id}/posts`}>{user.firstName} {user.lastName}</Link></li>
+        {user.email?<li className={styles.userdetails}>{user.email}</li>:<></>}
+        {user.birthday?<li className={styles.userdetails}>{user.birthday}</li>:<></>}
+        {user.highSchool?<li className={styles.userdetails}>{user.highSchool}</li>:<></>}
+        {user.college?<li className={styles.userdetails}>{user.college}</li>:<></>}
+        {user.currentCity?<li className={styles.userdetails}>{user.currentCity}</li>:<></>}
+        {user.hometown?<li className={styles.userdetails}>{user.hometown}</li>:<></>}
+        {user.relationshipStatus?<li className={styles.userdetails}>{user.relationshipStatus}</li>:<></>}
       </div>
-    </div>
   )
 }
 
