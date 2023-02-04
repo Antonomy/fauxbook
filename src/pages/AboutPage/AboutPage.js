64e55ff5228774ced250d50b878335dc8cd34faf
yevagav <yevagavrilov@gmail.com>
2023-02-03 21:19:10 -0800

    minor changes

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -5,7 +5,7 @@ import ProfileNavBar from '../../components/ProfileNavBar/ProfileNavBar'
 import editIcon from './images/editpencilicon.png'
 import { useParams } from 'react-router-dom'
 
-export default function AboutPage (user) {
+export default function AboutPage(user) {
   const [loggedInUser, setLoggedInUser] = useState(null)
   const [showSelfEditRights, setShowSelfEditRights] = useState(false)
   const [showFirstNameInput, setShowFirstNameInput] = useState(false)
@@ -47,7 +47,7 @@ export default function AboutPage (user) {
       console.error(error)
     }
   }
-  
+
 
   const handleUpdate = (evt) => {
     setFoundUser({ ...foundUser, [evt.target.name]: evt.target.value })
@@ -61,203 +61,205 @@ export default function AboutPage (user) {
 
   return (
 
-    <main>
+    <main className=''>
       <ProfileNavBar user={currentUser} />
-      <div>First Name: {user.firstName}
-        {showSelfEditRights
-          ? <>
-            <span onClick={(e) => { setShowFirstNameInput('true') }}>
-              <img className={styles.editIcon} height='10' src={editIcon} />
-            </span>
-            <input
-              style={{ display: showFirstNameInput ? 'block' : 'none' }}
-              type='text'
-              name='firstName'
-              onChange={handleUpdate}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  updateUser(user._id, { message: e.target.value })
-                  setShowFirstNameInput('false')
-                }
-              }}
-            />
+      <div className={styles.AboutPage}>
+        <h1>About</h1>
+        <div>First Name: {user.firstName}
+          {showSelfEditRights
+            ? <>
+              <span onClick={(e) => { setShowFirstNameInput('true') }}>
+                <img className={styles.editIcon} height='10' src={editIcon} />
+              </span>
+              <input
+                style={{ display: showFirstNameInput ? 'block' : 'none' }}
+                type='text'
+                name='firstName'
+                onChange={handleUpdate}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    updateUser(user._id, { message: e.target.value })
+                    setShowFirstNameInput('false')
+                  }
+                }}
+              />
             </>
-          : <></>}
-      </div>
-      <div>Last Name: {user.lastName}
-        {showSelfEditRights
-          ? <>
-            <span onClick={(e) => { setShowLastNameInput('true') }}>
-              <img className={styles.editIcon} height='10' src={editIcon} />
-            </span>
-            <input
-              style={{ display: showLastNameInput ? 'block' : 'none' }}
-              type='text'
-              name='lastName'
-              onChange={handleUpdate}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  updateUser(user._id, { message: e.target.value })
-                  setShowLastNameInput('false')
-                }
-              }}
-            />
+            : <></>}
+        </div>
+        <div>Last Name: {user.lastName}
+          {showSelfEditRights
+            ? <>
+              <span onClick={(e) => { setShowLastNameInput('true') }}>
+                <img className={styles.editIcon} height='10' src={editIcon} />
+              </span>
+              <input
+                style={{ display: showLastNameInput ? 'block' : 'none' }}
+                type='text'
+                name='lastName'
+                onChange={handleUpdate}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    updateUser(user._id, { message: e.target.value })
+                    setShowLastNameInput('false')
+                  }
+                }}
+              />
             </>
-          : <></>}
-      </div>
-      <div>Email: {user.email}
-        {showSelfEditRights
-          ? <>
-            <span onClick={(e) => { setShowEmailInput('true') }}>
-              <img className={styles.editIcon} height='10' src={editIcon} />
-            </span>
-            <input
-              style={{ display: showEmailInput ? 'block' : 'none' }}
-              type='text'
-              name='email'
-              onChange={handleUpdate}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  updateUser(user._id, { message: e.target.value })
-                  setShowEmailInput('false')
-                }
-              }}
-            />
+            : <></>}
+        </div>
+        <div>Email: {user.email}
+          {showSelfEditRights
+            ? <>
+              <span onClick={(e) => { setShowEmailInput('true') }}>
+                <img className={styles.editIcon} height='10' src={editIcon} />
+              </span>
+              <input
+                style={{ display: showEmailInput ? 'block' : 'none' }}
+                type='text'
+                name='email'
+                onChange={handleUpdate}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    updateUser(user._id, { message: e.target.value })
+                    setShowEmailInput('false')
+                  }
+                }}
+              />
             </>
-          : <></>}
-      </div>
+            : <></>}
+        </div>
 
-      <div>Birthday: {user.birthday}
-        {showSelfEditRights
-          ? <>
-            <span onClick={(e) => { setShowBirthdayInput('true') }}>
-              <img className={styles.editIcon} height='10' src={editIcon} />
-            </span>
-            <input
-              style={{ display: showBirthdayInput ? 'block' : 'none' }}
-              type='text'
-              name='birthday'
-              onChange={handleUpdate}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  updateUser(user._id, { message: e.target.value })
-                  setShowBirthdayInput('false')
-                }
-              }}
-            />
+        <div>Birthday: {user.birthday}
+          {showSelfEditRights
+            ? <>
+              <span onClick={(e) => { setShowBirthdayInput('true') }}>
+                <img className={styles.editIcon} height='10' src={editIcon} />
+              </span>
+              <input
+                style={{ display: showBirthdayInput ? 'block' : 'none' }}
+                type='text'
+                name='birthday'
+                onChange={handleUpdate}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    updateUser(user._id, { message: e.target.value })
+                    setShowBirthdayInput('false')
+                  }
+                }}
+              />
             </>
-          : <></>}
-      </div>
+            : <></>}
+        </div>
 
-      <div>High School: {user.highSchool}
-        {showSelfEditRights
-          ? <>
-            <span onClick={(e) => { setShowHighSchoolInput('true') }}>
-              <img className={styles.editIcon} height='10' src={editIcon} />
-            </span>
-            <input
-              style={{ display: showHighSchoolInput ? 'block' : 'none' }}
-              type='text'
-              name='highSchool'
-              onChange={handleUpdate}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  updateUser(user._id, { message: e.target.value })
-                  setShowHighSchoolInput('false')
-                }
-              }}
-            />
+        <div>High School: {user.highSchool}
+          {showSelfEditRights
+            ? <>
+              <span onClick={(e) => { setShowHighSchoolInput('true') }}>
+                <img className={styles.editIcon} height='10' src={editIcon} />
+              </span>
+              <input
+                style={{ display: showHighSchoolInput ? 'block' : 'none' }}
+                type='text'
+                name='highSchool'
+                onChange={handleUpdate}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    updateUser(user._id, { message: e.target.value })
+                    setShowHighSchoolInput('false')
+                  }
+                }}
+              />
             </>
-          : <></>}
-      </div>
-      <div>College: {user.college}
-        {showSelfEditRights
-          ? <>
-            <span onClick={(e) => { setShowCollegeInput('true') }}>
-              <img className={styles.editIcon} height='10' src={editIcon} />
-            </span>
-            <input
-              style={{ display: showCollegeInput ? 'block' : 'none' }}
-              type='text'
-              name='college'
-              onChange={handleUpdate}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  updateUser(user._id, { message: e.target.value })
-                  setShowCollegeInput('false')
-                }
-              }}
-            />
+            : <></>}
+        </div>
+        <div>College: {user.college}
+          {showSelfEditRights
+            ? <>
+              <span onClick={(e) => { setShowCollegeInput('true') }}>
+                <img className={styles.editIcon} height='10' src={editIcon} />
+              </span>
+              <input
+                style={{ display: showCollegeInput ? 'block' : 'none' }}
+                type='text'
+                name='college'
+                onChange={handleUpdate}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    updateUser(user._id, { message: e.target.value })
+                    setShowCollegeInput('false')
+                  }
+                }}
+              />
             </>
-          : <></>}
-      </div>
+            : <></>}
+        </div>
 
-      <div>Current City: {user.currentCity}
-        {showSelfEditRights
-          ? <>
-            <span onClick={(e) => { setShowCurrentCityInput('true') }}>
-              <img className={styles.editIcon} height='10' src={editIcon} />
-            </span>
-            <input
-              style={{ display: showCurrentCityInput ? 'block' : 'none' }}
-              type='text'
-              name='currentCity'
-              onChange={handleUpdate}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  updateUser(user._id, { message: e.target.value })
-                  setShowCurrentCityInput('false')
-                }
-              }}
-            />
+        <div>Current City: {user.currentCity}
+          {showSelfEditRights
+            ? <>
+              <span onClick={(e) => { setShowCurrentCityInput('true') }}>
+                <img className={styles.editIcon} height='10' src={editIcon} />
+              </span>
+              <input
+                style={{ display: showCurrentCityInput ? 'block' : 'none' }}
+                type='text'
+                name='currentCity'
+                onChange={handleUpdate}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    updateUser(user._id, { message: e.target.value })
+                    setShowCurrentCityInput('false')
+                  }
+                }}
+              />
             </>
-          : <></>}
-      </div>
+            : <></>}
+        </div>
 
-      <div>Home Town: {user.hometown}
-        {showSelfEditRights
-          ? <>
-            <span onClick={(e) => { setShowHometownInput('true') }}>
-              <img className={styles.editIcon} height='10' src={editIcon} />
-            </span>
-            <input
-              style={{ display: showHometownInput ? 'block' : 'none' }}
-              type='text'
-              name='hometown'
-              onChange={handleUpdate}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  updateUser(user._id, { message: e.target.value })
-                  setShowHometownInput('false')
-                }
-              }}
-            />
+        <div>Home Town: {user.hometown}
+          {showSelfEditRights
+            ? <>
+              <span onClick={(e) => { setShowHometownInput('true') }}>
+                <img className={styles.editIcon} height='10' src={editIcon} />
+              </span>
+              <input
+                style={{ display: showHometownInput ? 'block' : 'none' }}
+                type='text'
+                name='hometown'
+                onChange={handleUpdate}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    updateUser(user._id, { message: e.target.value })
+                    setShowHometownInput('false')
+                  }
+                }}
+              />
             </>
-          : <></>}
-      </div>
+            : <></>}
+        </div>
 
-      <div>Relationship Status: {user.relationshipStatus}
-        {showSelfEditRights
-          ? <>
-            <span onClick={(e) => { setShowRelationshipStatusInput('true') }}>
-              <img className={styles.editIcon} height='10' src={editIcon} />
-            </span>
-            <input
-              style={{ display: showRelationshipStatusInput ? 'block' : 'none' }}
-              type='text'
-              name='relationshipStatus'
-              onChange={handleUpdate}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  updateUser(user._id, { message: e.target.value })
-                  setShowRelationshipStatusInput('false')
-                }
-              }}
-            />
+        <div>Relationship Status: {user.relationshipStatus}
+          {showSelfEditRights
+            ? <>
+              <span onClick={(e) => { setShowRelationshipStatusInput('true') }}>
+                <img className={styles.editIcon} height='10' src={editIcon} />
+              </span>
+              <input
+                style={{ display: showRelationshipStatusInput ? 'block' : 'none' }}
+                type='text'
+                name='relationshipStatus'
+                onChange={handleUpdate}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    updateUser(user._id, { message: e.target.value })
+                    setShowRelationshipStatusInput('false')
+                  }
+                }}
+              />
             </>
-          : <></>}
+            : <></>}
+        </div>
       </div>
-
     </main>
   )
 }
